45edb2dda014750a1b5ab71609ca682bbf717048
jiangdunchun <jiangdunchun@outlook.com>
2022-11-14 20:45:46 +0800

    add ccd solver for part2(has bug)

--- lab1/Lab2_IK_answers.py
+++ lab1/Lab2_IK_answers.py
@@ -14,7 +14,53 @@ def part1_inverse_kinematics(meta_data, joint_positions, joint_orientations, tar
         joint_positions: 计算得到的关节位置，是一个numpy数组，shape为(M, 3)，M为关节数
         joint_orientations: 计算得到的关节朝向，是一个numpy数组，shape为(M, 4)，M为关节数
     """
-    
+    path, path_name, path_e2r, path_r2r = meta_data.get_path_from_root_to_end()
+    joint_parent = meta_data.joint_parent
+    joint_initial_position = meta_data.joint_initial_position
+
+    local_orientations = np.copy(joint_orientations)
+    end_position = joint_positions[path_e2r[0]]
+
+    for iter in range(32):
+        print("------------------->", iter)
+        for i in range(len(path_e2r) - 1):
+            p_index = path_e2r[i + 1]
+            p_position = joint_positions[p_index]
+
+            from_vec = end_position - p_position
+            to_vec = target_pose - p_position
+            end_position = p_position + np.linalg.norm(from_vec) * to_vec / np.linalg.norm(to_vec)
+
+            from_vec_norm = from_vec / np.linalg.norm(from_vec)
+            to_vec_norm = to_vec / np.linalg.norm(to_vec)
+            half = from_vec_norm + to_vec_norm
+            half = half / np.linalg.norm(half)
+            delta_qot_quat = np.array([0.0, 0.0, 0.0, 1.0])
+            cos_theta_2 = np.dot(from_vec_norm, half)
+            normal = np.cross(from_vec_norm, half)
+            sin_theta_2 = np.linalg.norm(normal)
+            if sin_theta_2 != 0:
+                normal = normal / sin_theta_2
+                delta_qot_quat[0] = normal[0] * sin_theta_2
+                delta_qot_quat[1] = normal[1] * sin_theta_2
+                delta_qot_quat[2] = normal[2] * sin_theta_2
+                delta_qot_quat[3] = cos_theta_2
+
+            local_orientations[p_index] = (R.from_quat(local_orientations[p_index]) * R.from_quat(delta_qot_quat)).as_quat()
+
+        for m_index in range(len(joint_parent)):
+            p_index = joint_parent[m_index]
+            if p_index >= 0:
+                offset = joint_initial_position[m_index] - joint_initial_position[p_index]
+                p_position = joint_positions[p_index]
+                joint_positions[m_index] = p_position + np.inner(R.from_quat(joint_orientations[p_index]).as_matrix(), offset)
+                joint_orientations[m_index] = (R.from_quat(joint_orientations[p_index]) * R.from_quat(local_orientations[m_index])).as_quat()
+
+        distance = np.linalg.norm(end_position - target_pose)
+        print("distance: ", distance)
+        if distance < 0.0001:
+            break
+
     
     return joint_positions, joint_orientations
 
